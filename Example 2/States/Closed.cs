d66f62b4b07b19f312e9f3cb466a13dbe4078711
Shadman Kudchikar <shadmanssk@yahoo.in>
2019-11-23 22:03:39 +0530

    Add project files.

---
+++ Example 2/States/Closed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2.States
+{
+    public class Closed : BaseState, ICommand
+    {
+        private readonly WorkItem owner;
+
+        public Closed(WorkItem owner) : base(owner)
+        {
+            this.owner = owner;
+        }
+
+        public void Open()
+        {
+            Console.WriteLine("Work Item is closed and cannot be modified.");
+        }
+
+        public void Resolve()
+        {
+            Console.WriteLine("Work Item is closed and cannot be modified.");
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("Work Item is already closed.");
+        }
+
+        public bool Delete()
+        {
+            return true;
+        }
+    }
+}
---
+++ Example 2/States/Proposed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2.States
+{
+    public class Proposed : BaseState, ICommand
+    {
+        private readonly WorkItem owner;
+
+        public Proposed(WorkItem owner) : base(owner)
+        {
+            this.owner = owner;
+        }
+
+        public void Open()
+        {
+            owner.State = StateEnum.Active;
+        }
+
+        public void Resolve()
+        {
+            Console.WriteLine("Work Item is in proposed state and cannot be directly resolved.");
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("Work Item is in proposed state and cannot be closed.");
+        }
+
+        public bool Delete()
+        {
+            return true;
+        }
+    }
+}
